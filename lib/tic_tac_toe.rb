01c6b23e883e850c9ca6afad3b37f3f78ac85526
Terrence Bell <terrencekbell@gmail.com>
2020-03-14 23:41:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,8 +54,13 @@ def won?(board)
   position_1 = combo[0]
   position_2 = combo[1]
   position_3 = combo[2]
- if board[position_1] == "X" && board[position_2] == "X" && board[position_3]
-
+ if board[position_1] == "X" && board[position_2] == "X" && board[position_3] == "X"
+   winning_combo = combo 
+   elsif board[position_1] == "O" && board[position_2] == "O" board[position_3] == "O"
+   winning_combo = combo 
+ end
+end 
+winning_combo
 end 
 
 
